a0a1ce70a6d02e3aa02f87484d073275e695f313
sterben <1579729633@qq.com>
2020-04-09 15:48:13 +0800

    2020/4/9

--- user/src/Filter/CIC/CIC_DOWN_S2.v
+++ user/src/Filter/CIC/CIC_DOWN_S2.v
@@ -1,5 +1,5 @@
 `timescale 1 ns / 1 ns
-module CIC_DOWN_S3 #(
+module CIC_DOWN_S2 #(
 	parameter INPUT_WIDTH  = 12,
 	parameter OUTPUT_WIDTH = 15
 ) (
@@ -13,8 +13,9 @@ module CIC_DOWN_S3 #(
 );
 
 localparam FILTER_WIDTH = OUTPUT_WIDTH;
+
 ////////////////////////////////////////////////////////////////
-//Module Architecture: CIC_DOWN_S3
+//Module Architecture: CIC_Down_S2
 ////////////////////////////////////////////////////////////////
   // Local Functions
   // Type Definitions
@@ -24,52 +25,38 @@ localparam FILTER_WIDTH = OUTPUT_WIDTH;
   wire phase_1; // boolean
   reg  ce_out_reg = 0; // boolean
   //   
-  reg  signed [INPUT_WIDTH-1:0] input_register = 0; // sfix12_En11
+  reg  signed [INPUT_WIDTH-1:0] input_register = 0; // sfix16_En15
   //   -- Section 1 Signals 
-  wire signed [INPUT_WIDTH-1:0] section_in1; // sfix12_En11
-  wire signed [FILTER_WIDTH-1:0] section_cast1; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] sum1; // sfix15_En11
-  reg  signed [FILTER_WIDTH-1:0] section_out1 = 0; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] add_cast; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] add_cast_1; // sfix15_En11
-  wire signed [FILTER_WIDTH:0] add_temp; // sfix16_En11
+  wire signed [INPUT_WIDTH-1:0] section_in1; // sfix16_En15
+  wire signed [FILTER_WIDTH-1:0] section_cast1; // sfix18_En15
+  wire signed [FILTER_WIDTH-1:0] sum1; // sfix18_En15
+  reg  signed [FILTER_WIDTH-1:0] section_out1 = 0; // sfix18_En15
+  wire signed [FILTER_WIDTH-1:0] add_cast; // sfix18_En15
+  wire signed [FILTER_WIDTH-1:0] add_cast_1; // sfix18_En15
+  wire signed [FILTER_WIDTH:0] add_temp; // sfix19_En15
   //   -- Section 2 Signals 
-  wire signed [FILTER_WIDTH-1:0] section_in2; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] sum2; // sfix15_En11
-  reg  signed [FILTER_WIDTH-1:0] section_out2 = 0; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] add_cast_2; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] add_cast_3; // sfix15_En11
-  wire signed [FILTER_WIDTH:0] add_temp_1; // sfix16_En11
+  wire signed [FILTER_WIDTH-1:0] section_in2; // sfix18_En15
+  wire signed [FILTER_WIDTH-1:0] sum2; // sfix18_En15
+  reg  signed [FILTER_WIDTH-1:0] section_out2 = 0; // sfix18_En15
+  wire signed [FILTER_WIDTH-1:0] add_cast_2; // sfix18_En15
+  wire signed [FILTER_WIDTH-1:0] add_cast_3; // sfix18_En15
+  wire signed [FILTER_WIDTH:0] add_temp_1; // sfix19_En15
   //   -- Section 3 Signals 
-  wire signed [FILTER_WIDTH-1:0] section_in3; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] sum3; // sfix15_En11
-  reg  signed [FILTER_WIDTH-1:0] section_out3 = 0; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] add_cast_4; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] add_cast_5; // sfix15_En11
-  wire signed [FILTER_WIDTH:0] add_temp_2; // sfix16_En11
+  wire signed [FILTER_WIDTH-1:0] section_in3; // sfix18_En15
+  reg  signed [FILTER_WIDTH-1:0] diff1 = 0; // sfix18_En15
+  wire signed [FILTER_WIDTH-1:0] section_out3; // sfix18_En15
+  wire signed [FILTER_WIDTH-1:0] sub_cast; // sfix18_En15
+  wire signed [FILTER_WIDTH-1:0] sub_cast_1; // sfix18_En15
+  wire signed [FILTER_WIDTH:0] sub_temp; // sfix19_En15
   //   -- Section 4 Signals 
-  wire signed [FILTER_WIDTH-1:0] section_in4; // sfix15_En11
-  reg  signed [FILTER_WIDTH-1:0] diff1 = 0; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] section_out4; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] sub_cast; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] sub_cast_1; // sfix15_En11
-  wire signed [FILTER_WIDTH:0] sub_temp; // sfix16_En11
-  //   -- Section 5 Signals 
-  wire signed [FILTER_WIDTH-1:0] section_in5; // sfix15_En11
-  reg  signed [FILTER_WIDTH-1:0] diff2 = 0; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] section_out5; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] sub_cast_2; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] sub_cast_3; // sfix15_En11
-  wire signed [FILTER_WIDTH:0] sub_temp_1; // sfix16_En11
-  //   -- Section 6 Signals 
-  wire signed [FILTER_WIDTH-1:0] section_in6; // sfix15_En11
-  reg  signed [FILTER_WIDTH-1:0] diff3 = 0; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] section_out6; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] sub_cast_4; // sfix15_En11
-  wire signed [FILTER_WIDTH-1:0] sub_cast_5; // sfix15_En11
-  wire signed [FILTER_WIDTH:0] sub_temp_2; // sfix16_En11
+  wire signed [FILTER_WIDTH-1:0] section_in4; // sfix18_En15
+  reg  signed [FILTER_WIDTH-1:0] diff2 = 0; // sfix18_En15
+  wire signed [FILTER_WIDTH-1:0] section_out4; // sfix18_En15
+  wire signed [FILTER_WIDTH-1:0] sub_cast_2; // sfix18_En15
+  wire signed [FILTER_WIDTH-1:0] sub_cast_3; // sfix18_En15
+  wire signed [FILTER_WIDTH:0] sub_temp_1; // sfix19_En15
   //   
-  reg  signed [FILTER_WIDTH-1:0] output_register = 0; // sfix15_En11
+  reg  signed [FILTER_WIDTH-1:0] output_register = 0; // sfix18_En15
 
   // Block Statements
   //   ------------------ CE Output Generation ------------------
@@ -163,89 +150,47 @@ localparam FILTER_WIDTH = OUTPUT_WIDTH;
       end
     end // integrator_delay_section2
 
-  //   ------------------ Section # 3 : Integrator ------------------
+  //   ------------------ Section # 3 : Comb ------------------
 
   assign section_in3 = section_out2;
 
-  assign add_cast_4 = section_in3;
-  assign add_cast_5 = section_out3;
-  assign add_temp_2 = add_cast_4 + add_cast_5;
-  assign sum3 = add_temp_2[FILTER_WIDTH-1:0];
-
-  always @ (posedge clk or posedge reset)
-    begin: integrator_delay_section3
-      if (reset == 1'b1) begin
-        section_out3 <= 0;
-      end
-      else begin
-        if (clk_enable == 1'b1) begin
-          section_out3 <= sum3;
-        end
-      end
-    end // integrator_delay_section3
-
-  //   ------------------ Section # 4 : Comb ------------------
-
-  assign section_in4 = section_out3;
-
-  assign sub_cast = section_in4;
+  assign sub_cast = section_in3;
   assign sub_cast_1 = diff1;
   assign sub_temp = sub_cast - sub_cast_1;
-  assign section_out4 = sub_temp[FILTER_WIDTH:0];
+  assign section_out3 = sub_temp[FILTER_WIDTH-1:0];
 
   always @ (posedge clk or posedge reset)
-    begin: comb_delay_section4
+    begin: comb_delay_section3
       if (reset == 1'b1) begin
         diff1 <= 0;
       end
       else begin
         if (phase_1 == 1'b1) begin
-          diff1 <= section_in4;
+          diff1 <= section_in3;
         end
       end
-    end // comb_delay_section4
+    end // comb_delay_section3
 
-  //   ------------------ Section # 5 : Comb ------------------
+  //   ------------------ Section # 4 : Comb ------------------
 
-  assign section_in5 = section_out4;
+  assign section_in4 = section_out3;
 
-  assign sub_cast_2 = section_in5;
+  assign sub_cast_2 = section_in4;
   assign sub_cast_3 = diff2;
   assign sub_temp_1 = sub_cast_2 - sub_cast_3;
-  assign section_out5 = sub_temp_1[FILTER_WIDTH-1:0];
+  assign section_out4 = sub_temp_1[FILTER_WIDTH-1:0];
 
   always @ (posedge clk or posedge reset)
-    begin: comb_delay_section5
+    begin: comb_delay_section4
       if (reset == 1'b1) begin
         diff2 <= 0;
       end
       else begin
         if (phase_1 == 1'b1) begin
-          diff2 <= section_in5;
+          diff2 <= section_in4;
         end
       end
-    end // comb_delay_section5
-
-  //   ------------------ Section # 6 : Comb ------------------
-
-  assign section_in6 = section_out5;
-
-  assign sub_cast_4 = section_in6;
-  assign sub_cast_5 = diff3;
-  assign sub_temp_2 = sub_cast_4 - sub_cast_5;
-  assign section_out6 = sub_temp_2[FILTER_WIDTH-1:0];
-
-  always @ (posedge clk or posedge reset)
-    begin: comb_delay_section6
-      if (reset == 1'b1) begin
-        diff3 <= 0;
-      end
-      else begin
-        if (phase_1 == 1'b1) begin
-          diff3 <= section_in6;
-        end
-      end
-    end // comb_delay_section6
+    end // comb_delay_section4
 
   //   ------------------ Output Register ------------------
 
@@ -256,7 +201,7 @@ localparam FILTER_WIDTH = OUTPUT_WIDTH;
       end
       else begin
         if (phase_1 == 1'b1) begin
-          output_register <= section_out6;
+          output_register <= section_out4;
         end
       end
     end // output_reg_process
@@ -264,4 +209,4 @@ localparam FILTER_WIDTH = OUTPUT_WIDTH;
   // Assignment Statements
   assign ce_out = ce_out_reg;
   assign filter_out = output_register;
-endmodule  // CIC_DOWN_S3
+endmodule  // CIC_Down_S2
